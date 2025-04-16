1272e062f2d2e1f8bd6dd431849a6808f06653ba
Dan Perkins <dperkins@zscaler.com>
2025-04-16 11:31:18 -0400

    fix: include version files in built deployment packages

--- deployment/azure/vm-applications/build-deployment-packages.sh
+++ deployment/azure/vm-applications/build-deployment-packages.sh
@@ -1,17 +1,17 @@
 filename="zscaler-microsegmentation-agent-windows.zip"
 echo "Building $filename"
-zip -j $filename windows/install.ps1 windows/uninstall.ps1 provision_key
+zip -j $filename windows/install.ps1 windows/uninstall.ps1 provision_key version
 shavalue=$(sha256sum $filename --quiet)
 echo "SHA256 value: $shavalue\n"
 
 filename="zscaler-microsegmentation-agent-linux-rpm.zip"
 echo "Building $filename"
-zip -j $filename linux/rpm/install.sh linux/rpm/uninstall.sh provision_key
+zip -j $filename linux/rpm/install.sh linux/rpm/uninstall.sh provision_key version
 shavalue=$(sha256sum $filename --quiet)
 echo "SHA256 value: $shavalue\n"
 
 filename="zscaler-microsegmentation-agent-linux-deb.zip"
 echo "Building $filename"
-zip -j $filename linux/deb/install.sh linux/deb/uninstall.sh provision_key
+zip -j $filename linux/deb/install.sh linux/deb/uninstall.sh provision_key version
 shavalue=$(sha256sum $filename --quiet)
 echo "SHA256 value: $shavalue\n"
