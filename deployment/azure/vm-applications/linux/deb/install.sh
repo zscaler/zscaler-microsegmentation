ebff3f306e3b7dd2f1aa2174bae2d2ef64701493
Dan Perkins <dperkins@zscaler.com>
2025-04-18 09:23:31 -0400

    feat: attempt wget and curl with default command options then fall back to basic calls if needed

--- deployment/azure/vm-applications/linux/deb/install.sh
+++ deployment/azure/vm-applications/linux/deb/install.sh
@@ -10,14 +10,40 @@ DIR="/opt/zscaler"
 mkdir -p $DIR/var
 mv -f $DIR/installation/provision_key $DIR/var
 
+# Check if wget is installed
 if command -v wget 2>&1 >/dev/null
 then
+    # Try wget with all relevant command options. This may fail with older versions of wget.
     wget -N --debug --secure-protocol=TLSv1_2 --tries=2 --retry-connrefused --retry-on-host-error --directory-prefix="$DIR/installation" $URL
+    if [ $? -ne 0 ]
+    then
+        # Exit code was nonzero. Attempt wget with fewer options.
+        echo "Failed to download the installer using default wget options. Attempting fall-back."
+        wget -N --debug --directory-prefix="$DIR/installation" $URL
+        if [ $? -ne 0 ]
+        then
+            echo "Failed all attempts to download installer using wget" >&2
+            exit 1
+        fi
+    fi
+# Check if curl is installed
 elif command -v curl 2>&1 >/dev/null
 then
+    # Try curl with all relevant command options. This may fail with older versions of curl.
     curl -v --tlsv1.2 --retry 2 --retry-all-errors --remote-name --create-dirs --output-dir "$DIR/installation" $URL
+    if [ $? -ne 0 ]
+    then
+        # Exit code was nonzero. Attempt curl with fewer options.
+        echo "Failed to download the installer using default curl options. Attempting fall-back."
+        curl -v --remote-name --create-dirs --output-dir "$DIR/installation" $URL
+        if [ $? -ne 0 ]
+        then
+            echo "Failed to download installer using curl" >&2
+            exit 1
+        fi
+    fi
 else
-    echo "Failed to download installer"
+    echo "Failed all attempts to download installer: curl and wget not available"
     exit 1
 fi
 
